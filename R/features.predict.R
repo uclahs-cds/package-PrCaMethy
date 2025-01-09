b3fe4cc844576a92bc0f3615705d191433121b90
Jaron Arbet <[jarbet@mednet.ucla.edu]>
2025-01-09 10:54:50 -0800

    rename features.predict

--- R/features.predict.R
+++ R/features.predict.R
@@ -1,4 +1,4 @@
-#' predict.features
+#' features.predict
 #'
 #' Main function used to predict various clinical and molecular features from gene-level methylation data in prostate cancer patients.
 #' @inheritParams validate.gene.methy.data
@@ -14,15 +14,15 @@
 #'check <- validate.gene.methy.data(example.data.gene.methy, example.models);
 #'stopifnot(check$val.passed);
 #'
-#'features <- predict.features(example.data.gene.methy, example.models);
+#'features <- features.predict(example.data.gene.methy, example.models);
 #'features;
-predict.features <- function(gene.methy.data, models) {
+features.predict <- function(gene.methy.data, models) {
     check <- validate.gene.methy.data(gene.methy.data, models);
     if (!check$val.passed) {
         print('Error: gene.methy.data is missing genes that are required for predicting features.  See the returned results for a list of missing genes per feature.')
         return(check);
         }
-    features <- lapply(models, function(x) predict(x, newdata = gene.methy.data));
+    features <- lapply(models, function(x) caret:::predict.train(x, newdata = gene.methy.data));
     features <- do.call(data.frame, features);
     stopifnot(all(rownames(features) == rownames(gene.methy.data)));
     return(features);
